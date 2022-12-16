df65ddcf8dc3dfc75796d63ee663584bdaa07a49
Devmalya De <devmalyade420@gmail.com>
2022-12-16 19:00:22 +0530

    first commit

---
+++ src/components/movieList/movieList.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from "react"
+import "./movieList.css"
+import { useParams } from "react-router-dom"
+import Cards from "../card/card"
+
+const MovieList = () => {
+    
+    const [movieList, setMovieList] = useState([])
+    const {type} = useParams()
+
+   
+
+    useEffect(() => {
+        getData()
+    }, [type])
+
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "upcoming"}?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieList(data.results))
+    }
+
+    return (
+        <div className="movie__list">
+            <h2 className="list__title">{(type ? type : "UPCOMING").toUpperCase()}</h2>
+            <div className="list__cards">
+                {
+                    movieList.map(movie => (
+                        <Cards movie={movie} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MovieList
